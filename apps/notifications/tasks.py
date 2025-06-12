4c9c0e7e1882aa2bdf62896497377af0c75acc32
Dileep Bandla <dileepb@rapyd.net>
2025-06-12 13:56:16 +0400

    notifications tasks added

--- apps/notifications/tasks.py
+++ apps/notifications/tasks.py
@@ -15,7 +15,9 @@ def mark_all_notifications_read(user_id, batch_size=5000):
 def delete_old_notifications(batch_size=2500):
     one_year_ago = timezone.now() - timedelta(days=365)
     while True:
-        old_qs = Notification.objects.filter(created_at__lt=one_year_ago)[:batch_size]
+        old_qs = Notification.objects.filter(created_at__lt=one_year_ago, read=True)[
+            :batch_size
+        ]
         count = old_qs.count()
         if not count:
             break
